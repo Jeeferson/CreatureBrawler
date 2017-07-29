d4f737000651efbea5ea6d363cbe3be2a573696b
Zach Wilkinson <Zach Wilkinson>
2017-07-29 11:05:01 -0500

    Added initial bits

---
+++ Fantastical_Deathmatch/ClassLibrary1/Goblin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Goblin 
+    {
+		//this is for makin the random nombers
+		private static Random _rng = new Random();
+
+		private int _hitPoints;
+		private string _name;
+		private bool _isDead;
+
+		public int GetHitPoints() { return _hitPoints; }
+		public int SetHitPoints(int hp) {
+			_hitPoints = hp + _rng.Next(10);
+			return _hitPoints;
+		}
+
+		public string GetName() { return _name; }
+		public string SetName(string name) { _name = name; return _name; }
+
+		public bool IsDead() { return _isDead; }
+
+		public void Attack(Goblin enemy)
+		{
+			int damage = _rng.Next(1,10);
+			enemy.Hit(damage);
+			Console.WriteLine($"{_name} attacks for {damage}.  {enemy.GetName()} has {enemy.GetHitPoints()} left");
+		}
+
+		private void Hit(int damage)
+		{
+			_hitPoints -= damage;
+			if (_hitPoints <= 0)
+			{
+				Console.WriteLine($"{_name} has died. RIP");
+				_isDead = true;
+			}
+		}
+
+	}
+}
---
+++ Fantastical_Deathmatch/Fantastical_Deathmatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fantastical_Deathmatch
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+		    Goblin g1 = new Goblin();
+
+			g1.SetName("Grobgar the Destroyer");
+			g1.SetHitPoints(40);
+
+			Goblin g2 = new Goblin();
+
+			g2.SetName("Xanzar the Dragonstabber");
+			g2.SetHitPoints(40);
+			Random _rng = new Random();
+			int coinflip = _rng.Next(1,2);
+
+			while (g1.IsDead() == false && g2.IsDead() == false)
+			{
+				Thread.Sleep(3000);
+				Console.Clear();
+				if (coinflip == 1) { g1.Attack(g2); coinflip = 2; }
+				else { g2.Attack(g1); coinflip = 1; }
+			}
+			Console.WriteLine("And we have a winnnnnneerrrrr!");
+			Console.ReadLine();
+		}
+	}
+}
